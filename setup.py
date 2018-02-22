f9e871e8ad66c530436e23a0d04199eca242fff8
DnrkasEFF <dnrkaseff360@gmail.com>
2018-02-21 21:35:41 -0500

    updating license extension and setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@ setup(
   name = 'censuspy',
   packages = ['censuspy'], 
   version = '0.1',
+  license='MIT',
   description = 'Lightweight wrapper to access gov data',
   author = 'Aadhi Manivannan',
   author_email = 'dnrkaseff360@gmail.com',
   url = 'https://github.com/DnrkasEFF/censuspy',
   download_url = 'https://github.com/DnrkasEFF/censuspy/archive/0.1.tar.gz',
   keywords = ['census', 'python', 'wrapper'],
+  install_requires=[
+          'requests',
+      ],
   classifiers = [],
 )
