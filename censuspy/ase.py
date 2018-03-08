d31e23f40548e9be0c388148e0117af14c70b4b8
DnrkasEFF <dnrkaseff360@gmail.com>
2018-03-07 20:19:34 -0500

    finish updates to annual survey of entrepreneurs module

--- censuspy/ase.py
+++ censuspy/ase.py
@@ -172,3 +172,83 @@ class cscb(object):
 
 			except:
 				return 'UNKN ERROR'
+
+class cscbo(object):
+
+	BASE_URL = 'https://api.census.gov/data/2014/ase/cscbo?'
+
+	def __init__(self, api_key=None, geo=None):
+		if (api_key == None or geo == None):
+			raise ValueError('Missing api_key or geo parameters.')
+
+		else:
+			self.api_key = api_key
+			self.geo = '&' + parse.urlencode({'for': geo}) + ':'
+
+	def get(self, metric=None, code='*', acqbus=None, asecbo=None, educ=None, hrswrkd=None, naics2012=None,
+		ownrage=None, pfnct=None, priorbus=None, prminc=None, usborncit=None, yracqbus=None):		
+		if (metric == None or code == None):
+			raise ValueError('Missing metric and/or code parameters.')
+
+		else:
+			self.code = str(code)
+			metric = metric.upper()
+			self.metric = 'get=' + metric
+			self.raw_params_dict = {
+
+				'ACQBUS': str(acqbus), 'ASECBO': str(asecbo), 'EDUC': str(educ), 'HRSWRKD': str(hrswrkd), 
+				'NAICS2012': str(naics2012), 'OWNRAGE': str(ownrage), 'PFNCT': str(pfnct), 'PRIORBUS': str(priorbus),
+				'PRMINC': str(prminc), 'USBORNCIT': str(usborncit), 'YRACQBUS': str(yracqbus), 'key': self.api_key
+
+				}
+
+		self.params_dict = {}
+		for k, v in self.raw_params_dict.items():
+			if v == 'None':
+				pass
+
+			else:
+				self.params_dict.update({k:v})
+
+		return self._basic_execution()
+
+	def _url_builder(self, base_url, metric):
+		return base_url + metric + self.geo + self.code + '&' + parse.urlencode(self.params_dict)
+
+	def _make_request(self, url):
+		try:
+			r = requests.get(url)
+			status = r.status_code
+
+			if status == 204:
+				return None
+
+			elif status == 200:
+				return r.json()
+
+			else:
+				return 'ERROR: ' + str(status)
+
+		except:
+			raise ValueError('Incorrect parameters led to invalid API call.')
+
+	def _basic_execution(self):
+		if (self.geo == '&for=state:' and self.code == '*'):
+			raise ValueError('Missing code parameter.')
+
+		else:
+			url = self._url_builder(self.BASE_URL, self.metric)
+			json = self._make_request(url)
+
+			try:
+				if json == None:
+					return 'N/A'
+
+				elif json[:5] == 'ERROR':
+					return json
+
+				else:
+					return json[1][0]
+
+			except:
+				return 'UNKN ERROR'
