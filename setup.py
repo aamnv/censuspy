68deb481530a66da78c717b2132dac8dc9ee4410
DnrkasEFF <dnrkaseff360@gmail.com>
2018-04-01 19:03:17 -0400

    updating setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 setup(
   name = 'censuspy',
   packages = ['censuspy'], 
-  version = '1.0.0',
+  version = '1.1.0',
   license='MIT',
   python_requires='>=3',
   description = 'Lightweight wrapper to access gov data',
   author = 'Aadhi Manivannan',
   author_email = 'dnrkaseff360@gmail.com',
   url = 'https://github.com/DnrkasEFF/censuspy',
-  download_url = 'https://github.com/DnrkasEFF/censuspy/archive/1.0.0.tar.gz',
+  download_url = 'https://github.com/DnrkasEFF/censuspy/archive/1.1.0.tar.gz',
   keywords = ['census', 'python', 'wrapper'],
   install_requires=[
           'requests',
